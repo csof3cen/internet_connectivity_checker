0afd12ea78ae54e1eb58def92daef8cf468f1cb3
Moustapha <88747372+csof3cen@users.noreply.github.com>
2023-05-07 23:25:38 +0000

    clean: internet_connectivity_checker.dart

--- lib/internet_connectivity_checker.dart
+++ lib/internet_connectivity_checker.dart
@@ -1,17 +1,5 @@
 library internet_connectivity_checker;
 
-import 'package:internet_connectivity_checker/src/connectivity_checker.dart';
-
-// main() {
-//   ConnectivityChecker(interval: const Duration(seconds: 2))
-//       .stream
-//       .listen((event) {
-//     print('${event}');
-//   });
-//
-//   ConnectivityChecker(interval: const Duration(seconds: 5))
-//       .stream
-//       .listen((event) {
-//     print('Stream Listener 2 : ${event}');
-//   });
-// }
+export 'package:internet_connectivity_checker/src/connectivity_status.dart';
+export 'package:internet_connectivity_checker/src/connectivity_builder.dart';
+export 'package:internet_connectivity_checker/src/connectivity_checker.dart';
